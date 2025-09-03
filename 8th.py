a31fac9eeeb20233c026afe2db211cf78a2186ec
charugoel2604 <goelcharu2006@gmail.com>
2025-09-03 12:32:00 +0530

    Python Operators

---
+++ 8th.py
@@ -0,0 +1,29 @@
+a = 10
+b = 2
+c = a +b
+print ("a: {} b: {} a+b: {}".format(a,b,c))
+c = a -b
+print ("a: {} b: {} a-b: {}".format(a,b,c))
+c = a * b
+print ("a: {} b: {} a*b: {}".format(a,b,c))
+c=a/ b
+print ("a: {} b: {} a/b: {}".format(a,b,c))
+c = a % b
+print ("a: {} b: {} a%b: {}".format(a,b,c))
+c = a**b
+print ("a: {} b: {} a**b: {}".format(a,b,c))
+c = a//b
+print ("a: {} b: {} a//b: {}".format(a,b,c))
+
+if ( a == b ):
+ print("a is equal to b")
+else:
+ print("a is not equal to b")
+if ( a != b ):
+ print("a is not equal to b")
+else:
+ print("a is equal to b")
+if ( a <b ):
+ print("a is less than b" )
+else:
+ print("a is not less than b")

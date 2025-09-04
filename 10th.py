6d0de83dd626ab19430618d1b1b0cb367b932706
charugoel2604 <goelcharu2006@gmail.com>
2025-09-04 14:27:45 +0530

    Shorthand

---
+++ 10th.py
@@ -0,0 +1,9 @@
+a=int(input('Enter the 1st number : '))
+print(a)
+b=int(input('\nEnter the 2nd number : '))
+print(b)
+if a>b :
+    print('a is largest ')
+else :
+    print('b is largest')    
+print('a') if a>b else print('b')

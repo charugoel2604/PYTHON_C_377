1fd6a75becdce29f771e1d41e4d7df931bfa564f
goelcharu2604 <goelcharu2006@gmail.com>
2025-09-11 15:01:40 +0530

    While loop

---
+++ 12th.py
@@ -0,0 +1,19 @@
+
+i=1
+while i<6 :
+    print(i)
+    i+=1
+i=1
+while i<6 :
+    
+    if i ==3 :
+        break
+    print(i)    
+    i+=1  
+j=0
+while j<6 :
+    j+=1
+    if j == 3 :
+        continue
+    
+    print(j)

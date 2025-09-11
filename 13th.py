65aae85e8654f5e9a05626d7519a8822bec08462
goelcharu2604 <goelcharu2006@gmail.com>
2025-09-11 15:04:58 +0530

    For loop

---
+++ 13th.py
@@ -0,0 +1,29 @@
+# While else
+i=1
+while i<6:
+    print(i)
+    i+=1
+else:
+    print("Finally")    
+#for if 
+fruits=["Apple","banana","Cherry"]
+for y in fruits:
+    
+    if y=="Cherry" :
+       break
+    print(y)
+# for loop
+for c in range(2,6)   :
+    print(c)
+# for loop with increment of 3 step    
+for n in range(2,30,3):
+    print(n)        
+#
+for x in [0,1,2,3]:
+    pass    
+fruits=["Apple","banana","Cherry"]
+for y in fruits:
+    
+    if y=="Cherry" :
+       continue
+    print(y)
